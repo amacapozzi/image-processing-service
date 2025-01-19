e2b42bdf9ddc1cdd82ef7d7a5b2a5de5dc7ec445
amacapozzi <amacapozzi07@gmail.com>
2025-01-19 02:49:00 -0300

    setup routes

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"image-processor-server/routes"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	app := fiber.New()
+
+	routes.SetupImageRoutes(app)
+
 	app.Listen(":3000")
 }
