ddb63c94f06497d4a292eeec871f27e1bcc484ec
amacapozzi <amacapozzi07@gmail.com>
2025-01-19 03:07:31 -0300

    fix image rotate error

--- controllers/image.controller.go
+++ controllers/image.controller.go
@@ -1,7 +1,10 @@
 package controllers
 
 import (
+	"bytes"
+	"fmt"
 	"image-processor-server/services"
+	"io"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -15,30 +18,37 @@ func HandleUploadImage(c *fiber.Ctx) error {
 	}
 
 	multipartFile, err := file.Open()
-
 	if err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"error": err.Error(),
 		})
 	}
-
 	defer multipartFile.Close()
 
-	fileBuffer := make([]byte, 512)
-
-	if _, err := multipartFile.Read(fileBuffer); err != nil {
+	fileBuffer := &bytes.Buffer{}
+	if _, err := io.Copy(fileBuffer, multipartFile); err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"error": "Failed to read image" + err.Error(),
+			"error": "Failed to read image: " + err.Error(),
 		})
 	}
 
-	IS_VALID := services.IsValidImage(fileBuffer)
+	head := fileBuffer.Bytes()[:512]
 
-	if !IS_VALID {
+	if !services.IsValidImage(head) {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"error": "Invalid image type",
 		})
 	}
+
+	fmt.Println("Tamaño del archivo:", len(fileBuffer.Bytes()))
+	img, err := services.Rotate(fileBuffer.Bytes())
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Failed to process image: " + err.Error(),
+		})
+	}
+
+	fmt.Println(img.ColorModel())
 
 	return c.Status(fiber.StatusOK).SendString("Image upload successful")
 }
--- services/image.service.go
+++ services/image.service.go
@@ -1,9 +1,20 @@
 package services
 
 import (
+	"bytes"
+	"image"
+	"image/png"
+
 	"github.com/h2non/filetype"
 )
 
 func IsValidImage(imgBytes []byte) bool {
 	return filetype.IsImage(imgBytes)
+}
+
+func Rotate(imgBytes []byte) (image image.Image, err error) {
+
+	img, err := png.Decode(bytes.NewReader(imgBytes))
+
+	return img, err
 }
