8e97d3d977e2f1b993af0cc8c4f05da3a92cc02c
amacapozzi <amacapozzi07@gmail.com>
2025-01-19 15:48:48 -0300

    add function to parse angle

--- services/image.service.go
+++ services/image.service.go
@@ -2,9 +2,11 @@ package services
 
 import (
 	"bytes"
-	"image"
+	"image/color"
 	"image/png"
+	"math"
 
+	"github.com/disintegration/imaging"
 	"github.com/h2non/filetype"
 )
 
@@ -12,9 +14,32 @@ func IsValidImage(imgBytes []byte) bool {
 	return filetype.IsImage(imgBytes)
 }
 
-func Rotate(imgBytes []byte) (image image.Image, err error) {
-
+func Rotate(imgBytes []byte, angle float64, direction string) ([]byte, error) {
 	img, err := png.Decode(bytes.NewReader(imgBytes))
+	if err != nil {
+		return nil, err
+	}
 
-	return img, err
+	angle = normalizeAngle(angle)
+
+	if direction == "counterclockwise" {
+		angle = -angle
+	}
+
+	rotatedImage := imaging.Rotate(img, angle, color.Transparent)
+
+	var buf bytes.Buffer
+	if err := png.Encode(&buf, rotatedImage); err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
+
+func normalizeAngle(angle float64) float64 {
+	normalized := math.Mod(angle, 360)
+	if normalized < 0 {
+		normalized += 360
+	}
+	return normalized
 }
