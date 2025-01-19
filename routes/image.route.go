8193bf9d98eb573db477fa5de15962c5c414e0ce
amacapozzi <amacapozzi07@gmail.com>
2025-01-19 02:48:53 -0300

    add  routes

---
+++ routes/image.route.go
@@ -0,0 +1,11 @@
+package routes
+
+import (
+	"image-processor-server/controllers"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func SetupImageRoutes(app *fiber.App) {
+	app.Post("/image/upload", controllers.HandleUploadImage)
+}
