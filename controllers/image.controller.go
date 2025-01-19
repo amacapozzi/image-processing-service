9863a9c47dfafcb4050f935b839dec4d0af18add
amacapozzi <amacapozzi07@gmail.com>
2025-01-19 17:39:59 -0300

    add function to resize image

--- controllers/image.controller.go
+++ controllers/image.controller.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"bytes"
+	"image-processor-server/errors"
 	"image-processor-server/services"
 	"io"
 	"strconv"
@@ -12,9 +13,7 @@ import (
 func HandleRotateImage(c *fiber.Ctx) error {
 	file, err := c.FormFile("image")
 	if err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"error": err.Error(),
-		})
+		errors.HandleBadRequest(c, err)
 	}
 
 	angleValue := c.Params("angle")
@@ -22,17 +21,13 @@ func HandleRotateImage(c *fiber.Ctx) error {
 
 	multipartFile, err := file.Open()
 	if err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"error": err.Error(),
-		})
+		errors.HandleBadRequest(c, err)
 	}
 	defer multipartFile.Close()
 
 	fileBuffer := &bytes.Buffer{}
 	if _, err := io.Copy(fileBuffer, multipartFile); err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"error": "Failed to read image " + err.Error(),
-		})
+		errors.HandleBadRequest(c, err)
 	}
 
 	head := fileBuffer.Bytes()[:512]
@@ -45,12 +40,53 @@ func HandleRotateImage(c *fiber.Ctx) error {
 
 	rotatedImgBytes, err := services.Rotate(fileBuffer.Bytes(), float64(angleInt), "rotate")
 	if err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"error": "Failed to process image " + err.Error(),
-		})
+		errors.HandleBadRequest(c, err)
 	}
 
 	c.Set("Content-Type", "image/png")
 	return c.Send(rotatedImgBytes)
+
+}
+
+func HandleGrayScale(c *fiber.Ctx) error {
+
+	file, err := c.FormFile("image")
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	formData, err := c.MultipartForm()
+
+	if err != nil {
+		errors.HandleBadRequest(c, err)
+	}
+
+	multipartFile, err := file.Open()
+	if err != nil {
+		errors.HandleBadRequest(c, err)
+	}
+	defer multipartFile.Close()
+
+	var imgBuffer bytes.Buffer
+
+	if _, err := io.Copy(&imgBuffer, multipartFile); err != nil {
+		errors.HandleBadRequest(c, err)
+	}
+
+	widthStr, heightStr := formData.Value["width"][0], formData.Value["height"][0]
+
+	width, _ := strconv.Atoi(widthStr)
+	height, _ := strconv.Atoi(heightStr)
+
+	resizedImage, err := services.Resize(imgBuffer.Bytes(), width, height)
+
+	if err != nil {
+		errors.HandleBadRequest(c, err)
+	}
+
+	c.Set("Content-Type", "image/png")
+	return c.Send(resizedImage)
 
 }
